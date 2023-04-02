70932bc4b8a37c42df432fd7e5283ac660a80a3b
unknown <tyagipriyanshi79@gmail.com>
2023-04-02 15:14:56 -0700

    register-page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,14 @@ import {Route, Routes} from "react-router-dom";
 import IndexPage from "./pages/indexPage";
 import LoginPage from "./pages/LoginPage";
 import Layout from "./Layout";
+import RegisterPage from "./pages/RegisterPage";
 function App() {
   return (
     <Routes>
      <Route path="/" element={<Layout />}>
       <Route index element={<IndexPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage/>}/>
       </Route>
     </Routes>
   )
